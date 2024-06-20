8180ff4bdc57a90fa337571aa2705cc142414ce9
Kadu <kadu2002old@gmail.com>
2024-06-19 21:45:39 -0300

    version-1.2

--- db.php
+++ db.php
@@ -1,13 +1,13 @@
 <?php
 $servername = "localhost";
-$username = "root";  // Altere se necessário
-$password = "";      // Altere se necessário
+$username = "root";  
+$password = "";      
 $dbname = "CRUD_Library";
 
-// Criar conexão
+
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Verificar conexão
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
